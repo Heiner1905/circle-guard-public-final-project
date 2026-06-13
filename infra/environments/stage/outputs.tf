e42cc0ba258db81768f6a4010bbece9ef6cc488f
karoldmejia <karoldmejiaoz@gmail.com>
2026-06-13 07:13:58 -0500

    Format all terraform files

--- infra/environments/stage/outputs.tf
+++ infra/environments/stage/outputs.tf
@@ -1,69 +1,54 @@
 output "resource_group" {
-  description = "Name of the Azure Resource Group hosting this environment."
+  description = "Name of the resource group"
   value       = data.azurerm_resource_group.this.name
 }
 
 output "aks_cluster_name" {
-  description = "Name of the AKS cluster."
-  value       = module.aks.cluster_name
+  description = "Name of the AKS cluster (reusing dev cluster)"
+  value       = data.azurerm_kubernetes_cluster.this.name
 }
 
 output "acr_login_server" {
-  description = "ACR login server (push/pull endpoint)."
-  value       = module.acr.login_server
+  description = "ACR login server (reusing dev ACR)"
+  value       = data.azurerm_container_registry.this.login_server
 }
 
 output "middleware_namespace" {
-  description = "Kubernetes namespace where middleware lives."
-  value       = module.middleware.namespace
-}
-
-output "kafka_bootstrap" {
-  description = "In-cluster Kafka bootstrap server."
-  value       = module.middleware.kafka_bootstrap
+  description = "Kubernetes namespace where middleware is deployed"
+  value       = var.middleware_namespace
 }
 
 output "observability_namespace" {
-  description = "Namespace running Prometheus / Grafana (null when disabled)."
-  value       = var.enable_observability ? module.observability[0].namespace : null
-}
-
-output "grafana_service" {
-  description = "In-cluster DNS of Grafana (null when disabled). Port-forward to view dashboards."
-  value       = var.enable_observability ? module.observability[0].grafana_service : null
+  description = "Kubernetes namespace for observability stack"
+  value       = var.observability_namespace
 }
 
 output "prometheus_service" {
-  description = "In-cluster DNS of Prometheus (null when disabled)."
-  value       = var.enable_observability ? module.observability[0].prometheus_service : null
+  description = "Prometheus service DNS name"
+  value       = "kube-prometheus-stack-prometheus.${var.observability_namespace}.svc.cluster.local:9090"
+}
+
+output "grafana_service" {
+  description = "Grafana service DNS name"
+  value       = "kube-prometheus-stack-grafana.${var.observability_namespace}.svc.cluster.local:80"
 }
 
 output "loki_service" {
-  description = "In-cluster DNS of Loki (null when disabled). Add as Grafana datasource."
-  value       = var.enable_logging ? module.logging[0].loki_service : null
-}
-
-output "kibana_service" {
-  description = "In-cluster DNS of Kibana (null when ELK is disabled)."
-  value       = var.enable_logging && var.enable_elk ? module.logging[0].kibana_service : null
+  description = "Loki service DNS name"
+  value       = "loki-stack.${var.observability_namespace}.svc.cluster.local:3100"
 }
 
 output "jaeger_query_url" {
-  description = "In-cluster DNS of the Jaeger query UI (null when tracing is disabled)."
-  value       = var.enable_tracing ? module.tracing[0].jaeger_query_url : null
+  description = "Jaeger query service URL"
+  value       = "http://jaeger-query.${var.observability_namespace}.svc.cluster.local:16686"
 }
 
 output "jaeger_otlp_endpoint" {
-  description = "OTLP HTTP endpoint for Spring Boot tracers (null when tracing is disabled)."
-  value       = var.enable_tracing ? module.tracing[0].jaeger_otlp_http_endpoint : null
+  description = "Jaeger OTLP endpoint for traces"
+  value       = "http://jaeger-collector.${var.observability_namespace}.svc.cluster.local:4318/v1/traces"
 }
 
-output "cluster_issuer_name" {
-  description = "Name of the ClusterIssuer to reference in Ingress annotations (null when security or issuer is disabled)."
-  value       = var.enable_security ? module.security[0].cluster_issuer_name : null
-}
-
-output "psa_levels" {
-  description = "Pod Security Admission levels effectively applied to the application namespace (null when security is disabled)."
-  value       = var.enable_security ? module.security[0].psa_levels : null
+output "kafka_bootstrap" {
+  description = "Kafka bootstrap server address"
+  value       = "kafka.${var.middleware_namespace}.svc.cluster.local:9092"
 }
